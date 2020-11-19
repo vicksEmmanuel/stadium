a99c451217067761c040f3322c5885ba20ca6f42
vicksemmanuel <vicksemmanuel58@gmail.com>
2020-11-19 14:13:43 +0100

    Set up client

---
+++ Client/stadium/src/Components/TestingSpeech.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class TestingSpeech {
+    render() {
+        return <div>hello world</div>
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -115,6 +115,8 @@ namespace server
             }
 
             app.UseHttpsRedirection();
+            
+            app.UseStaticFiles();
 
             app.UseRouting();
             
